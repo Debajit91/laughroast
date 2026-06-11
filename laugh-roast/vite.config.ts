a336885158dd1f258ee450d35db4fc836f2d9c0e
Debajit91 <debajitroy544@gmail.com>
2026-06-12 03:23:45 +0600

    favicon added

--- laugh-roast/vite.config.ts
+++ laugh-roast/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
         start_url: "/",
         icons: [
           {
-            src: "/icon-192.png",
+            src: "./icon-192.png",
             sizes: "192x192",
             type: "image/png"
           },
